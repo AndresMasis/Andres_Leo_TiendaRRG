afa0bbce6d58ea66b9d9ea4d8638394bb075dedd
AndresMasis <67446603+AndresMasis@users.noreply.github.com>
2020-09-22 21:40:16 -0600

    Se actualzaron Items y Main

--- Items.java
+++ Items.java
@@ -1,10 +1,12 @@
 package andres_masis_poo;
-class Items{
+
+public class Items {
+
     double precio;
     String categoria;
     String nombre;
     double precioVen;
-   
+
     public Items(final double precio, final String categoria, final String nombre) {
         this.precio = precio;
         this.categoria = categoria;
@@ -21,16 +23,16 @@ class Items{
     }
 
     public String getNombre() {
-        System.out.println("Nombre: "+this.nombre);
+        System.out.println("Nombre: " + this.nombre);
         return this.nombre;
     }
 
     public void getCategoria() {
-        System.out.println("Categoria: "+this.categoria);
+        System.out.println("Categoria: " + this.categoria);
     }
 
     public void getPrecio() {
-        System.out.println("Precio: "+this.precio);
+        System.out.println("Precio: " + this.precio);
     }
 
     public void setItems(final double precio, final String categoria, final String nombre) {
@@ -42,69 +44,60 @@ class Items{
 
 class Armas extends Items { // atributo opcional durabilidad
 
-    public Armas(final double precio, final String categoria, final String nombre) 
-    {
+    public Armas(final double precio, final String categoria, final String nombre) {
         super(precio, categoria, nombre);
     }
 
     double daño;
     String tipoArma;
 
-    public void setArma(final double daño, final String tipoArma) 
-    {
+    public void setArma(final double daño, final String tipoArma) {
         this.daño = daño;
         this.tipoArma = tipoArma;
     }
 
-    public void getArma()
-    {
-        System.out.println("Daño: "+this.daño);
-        System.out.println("Tipo: "+this.tipoArma);
+    public void getArma() {
+        System.out.println("Daño: " + this.daño);
+        System.out.println("Tipo: " + this.tipoArma);
     }
 
 }
 
 class Armadura extends Items { // atributo opcional durabilidad
 
-    public Armadura(final double precio, final String categoria, final String nombre) 
-    {
+    public Armadura(final double precio, final String categoria, final String nombre) {
         super(precio, categoria, nombre);
     }
 
     double defensa;
     String tipoArma;
 
-    public void setArm(final double defensa, final String tipoArma) 
-    {
+    public void setArm(final double defensa, final String tipoArma) {
         this.defensa = defensa;
         this.tipoArma = tipoArma;
     }
 
-    public void getArmadura()
-    {
-        System.out.println("Defensa: "+this.defensa);
-        System.out.println("Tipo: "+this.tipoArma);
-        System.out.println("Tipo: "+this.precioVen);
+    public void getArmadura() {
+        System.out.println("Defensa: " + this.defensa);
+        System.out.println("Tipo: " + this.tipoArma);
+        System.out.println("Tipo: " + this.precioVen);
     }
 
 }
 
 class Consumibles extends Items { // atributo opcional cantidad
 
-    public Consumibles(final double precio, final String categoria, final String nombre) 
-    {
+    public Consumibles(final double precio, final String categoria, final String nombre) {
         super(precio, categoria, nombre);
     }
 
     String tipoConsu;
 
-    public void setCon(final String tipoConsu)
-    {
-        this.tipoConsu =  tipoConsu;
+    public void setCon(final String tipoConsu) {
+        this.tipoConsu = tipoConsu;
     }
 
-    public void getCon()
-    {
-        System.out.println("Tipo: "+this.tipoConsu);
+    public void getCon() {
+        System.out.println("Tipo: " + this.tipoConsu);
     }
- }
+}
--- Main.java
+++ Main.java
@@ -1,87 +1,117 @@
 package andres_masis_poo;
+
 import andres_masis_poo.Items;
 import java.util.Scanner;
+import java.util.LinkedList;
 
-class Main{
-    
-   public static void main(final String[] args) {
+public class Main {
 
-        System.out.println("------------------ITEM-----------------------");
+    public static void main(final String[] args) {
+        LinkedList<Items> todos_objetos = new LinkedList<>();
+        Scanner in = new Scanner(System.in);
+
+        //System.out.println("------------------ITEM-----------------------");
         Items primero;
-        primero = new Items(100,"Armas","Luci");
-        primero.getNombre();
+        primero = new Items(100, "Armas", "Luci");
+        /*primero.getNombre();
         primero.getCategoria();
-        primero.getPrecio();
-        
-        
-        System.out.println("-------------------ARMA----------------------");
+        primero.getPrecio();*/
+        todos_objetos.add(primero);
+
+        //System.out.println("-------------------ARMA----------------------");
         Armas uno;
-        uno = new Armas(100,"Armas","Slider");
-        uno.setArma(95,"Espada");
-        uno.getNombre();
+        uno = new Armas(100, "Armas", "Slider");
+        uno.setArma(95, "Espada");
+        /*uno.getNombre();
         uno.getCategoria();
         uno.getPrecio();
-        uno.getArma();
-        
-        
-        System.out.println("--------------------ARMADURA---------------------");
+        uno.getArma();*/
+        todos_objetos.add(uno);
+
+        //System.out.println("--------------------ARMADURA---------------------");
         Armadura dos;
-        dos = new Armadura(100,"Armadura","Chaleco debil");
-        dos.setArm(56,"Hombrera");
-        dos.getNombre();
+        dos = new Armadura(100, "Armadura", "Chaleco debil");
+        dos.setArm(56, "Hombrera");
+        /*dos.getNombre();
         dos.getCategoria();
         dos.getPrecio();
-        dos.getArmadura();
-        
-        
-        
-        System.out.println("-------------------CONSUMIBLE----------------------");
+        dos.getArmadura();*/
+        todos_objetos.add(dos);
+
+        //System.out.println("-------------------CONSUMIBLE----------------------");
         Consumibles tres;
-        tres = new Consumibles(25,"Consumibles","Pocion pequeña");
+        tres = new Consumibles(25, "Consumibles", "Pocion pequeña");
         tres.setCon("Pocion de vida");
-        tres.getNombre();
+        /*tres.getNombre();
         tres.getCategoria();
         tres.getPrecio();
-        tres.getCon();
-        
-        
-        System.out.println("-----------------------------------------");
-        
-        
-        
-        
-        System.out.println("Personaje");
-        Scanner in = new Scanner(System.in);
+        tres.getCon();*/
+        todos_objetos.add(tres);
+
+        //System.out.println("----------------------PERSONAJE-----------------------");
         Personaje character = new Personaje();
-        
-        int opcion;
-        
+
+        int opcion_accion, opcion_objeto, cantidad_objetos;
+        cantidad_objetos = todos_objetos.size();
+
         do {
-            System.out.println("¿Que deseas hacer? \n 1. Comprar"
-                    + "\n 2. Vender"
-                    + "\n 3. Equipar "
-                    + "\n 4. Desequipar "
-                    + "\n 5. Mostar invetario"
-                    + "\n 6. Salir");
-            opcion = in.nextInt();
+            opcion_accion = 0;
             
-            if(opcion == 1){
-                character.comprar(dos);
-            } else if (opcion == 2){
-                character.vender(dos);
-            } else if (opcion == 3){
-                character.equipar(dos);
-            } else if (opcion == 4){
-                character.desequipar(dos);
-            } else if (opcion == 5){
-                character.mostrarInventario();
-            } else {
-                System.out.println("Opcion invalida");
+            // Imprime el nombre de TODOS los objetos
+            for (int i = 0; i < cantidad_objetos; i++) {
+                System.out.println(i + 1 + ". " + todos_objetos.get(i).nombre);
+            }
+
+            // Interactuar con el obejto
+            System.out.println("\n¿Con cual objeto deseas interactuar?");
+            opcion_objeto = in.nextInt() - 1;
+            
+            
+            
+            // Validaciones
+            if (opcion_objeto == 9) {
+                // Salir
+                opcion_accion = 6;
+            } else if(opcion_objeto < 0 || opcion_objeto >= cantidad_objetos) {
+               System.out.println("Opcion invalida");
+               opcion_accion = 6; 
             }
             
-        } while (opcion != 6);
-        
-        
-    
+            
+            // Opciones
+            while (opcion_accion != 6) {
+                System.out.println("¿Que deseas hacer con " + todos_objetos.get(opcion_objeto).nombre + "?\n\n"
+                        + "\n 1. Comprar"
+                        + "\n 2. Vender"
+                        + "\n 3. Equipar "
+                        + "\n 4. Desequipar "
+                        + "\n 5. Mostar invetario"
+                        + "\n 6. Salir");
+                opcion_accion = in.nextInt();
+
+                switch (opcion_accion) {
+                    case 1:
+                        character.comprar(todos_objetos.get(opcion_objeto));
+                        break;
+                    case 2:
+                        character.vender(todos_objetos.get(opcion_objeto));
+                        break;
+                    case 3:
+                        character.equipar(todos_objetos.get(opcion_objeto));
+                        break;
+                    case 4:
+                        character.desequipar(todos_objetos.get(opcion_objeto));
+                        break;
+                    case 5:
+                        character.mostrarInventario();
+                        break;
+                    default:
+                        System.out.println("Opcion invalida");
+                        break;
+                }
+
+            }
+        } while (opcion_objeto != 9);
+
     }
 }
